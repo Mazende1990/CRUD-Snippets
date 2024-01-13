68fe08febf881b28fd17c1cfdfe960edfc8bf917
md222zv <89812908+Mazender@users.noreply.github.com>
2024-01-13 16:15:42 +0100

    Add snippet router and html

---
+++ src/routes/snippetRouter.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { SnippetController } from '../controllers/snippetController.js'
+
+export const router = express.Router()
+
+const controller = new SnippetController()
+
+// Provide req.doc to the route if :id is present in the route path.
+router.param('id', (req, res, next, id) => controller.loadTaskDocument(req, res, next, id))
+
+// Map HTTP verbs and route paths to controller action methods.
+router.get('/', (req, res, next) => controller.index(req, res, next))
+
+router.get('/create', (req, res, next) => controller.create(req, res, next))
+router.post('/create', (req, res, next) => controller.createPost(req, res, next))
+
+router.get('/:id/update', (req, res, next) => controller.update(req, res, next))
+router.post('/:id/update', (req, res, next) => controller.updatePost(req, res, next))
+
+router.get('/:id/delete', (req, res, next) => controller.delete(req, res, next))
+router.post('/:id/delete', (req, res, next) => controller.deletePost(req, res, next))
